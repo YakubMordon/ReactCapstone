2eb491744f74f81ae70572c8599eedae9a5ef31d
Jacob Mordon <yakubkazimiezh.mordon.kn.2021@lpnu.ua>
2024-01-31 16:16:24 +0200

    Added Reducer

--- src/App.js
+++ src/App.js
@@ -6,15 +6,31 @@ import Header from './components/Header';
 import Main from './components/Main';
 import './App.css';
 
+const availableTimesReducer = (state, action) => {
+  switch (action.type) {
+    case 'UPDATE_TIMES':
+      return ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00'];
+
+    default:
+      return state;
+  }
+};
+
+const updateTimes = (selectedDate) => ({ type: 'UPDATE_TIMES', payload: { selectedDate } });
+
 function App() {
-  const [availableTimes, setAvailableTimes] = useState(['17:00', '18:00', '19:00', '20:00', '21:00', '22:00']);
+  const [availableTimes, dispatch] = useReducer(availableTimesReducer, []);
+
+  const initializeTimes = () => {
+    dispatch(updateTimes());
+  };
 
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path='/' element={<Main />} />
-        <Route path='/booking' element={<Booking availableTimes={availableTimes}/>} />
+        <Route path='/' element={<Main initializeTimes={initializeTimes} />} />
+        <Route path='/booking' element={<Booking availableTimes={availableTimes} dispatch={dispatch}/>} />
       </Routes>
       <Footer />
     </Router>
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,15 @@
+import { useEffect } from 'react';
 import Testimonials from './Testimonials';
 import Specials from './Specials';
 import About from './About';
 import Hero from './Hero';
 import './Main.css';
 
-export default function Main() {
+export default function Main({initializeTimes}) {
+  useEffect(()=>{
+    initializeTimes();
+  },[])
+  
   return (
     <main>
       <Hero />
